28f50e06e49f23a15b818c0923166d8cda439019
picturestone <lukas.bildstein@outlook.com>
2024-05-01 23:37:02 +0200

    Fixed bug when adding entries to penalty points, hid bets from user detail view

--- src/Controller/UserResultBetsController.php
+++ src/Controller/UserResultBetsController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Season;
 use App\Entity\User;
 use App\Repository\SeasonRepository;
 use App\Repository\UserRepository;
+use App\ScoreCalculation\RaceScoreCalculator;
+use App\ScoreCalculation\ResultsForUser;
 use App\ScoreCalculation\ScoreCalculationService;
+use DateTimeZone;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -49,6 +53,12 @@ class UserResultBetsController extends AbstractController
     #[Route('/user-result-bets/{id}', name: 'app_user_result_bets_detail', methods: ['GET'])]
     public function detail(Request $request, $id): Response
     {
+        /** @var \App\Entity\User $user */
+        $user = $this->getUser();
+        if (!$user) {
+            return throw $this->createAccessDeniedException('Must be logged in for this operation');
+        }
+
         $activeSeasons = $this->seasonRepository->findBy(['isActive' => true]);
 
         if (!$activeSeasons) {
@@ -56,19 +66,25 @@ class UserResultBetsController extends AbstractController
         }
 
         $season = $activeSeasons[0];
-        $user = $this->userRepository->find($id);
+        $forUser = $this->userRepository->find($id);
 
-        if (!$user) {
+        if (!$forUser) {
             return throw $this->createNotFoundException('This user does not exist');
         }
 
         $scoreCalculator = new ScoreCalculationService($this->seasonRepository, $this->userRepository, $season);
-        $resultsForUser = $scoreCalculator->getResultsForUser($user);
+        $resultsForUser = $scoreCalculator->getResultsForUser($forUser);
+
+        $now = new \DateTimeImmutable('now', new DateTimeZone('UTC'));
+        $bettingTimeLimit = $this->getParameter('F1SONNTAG_BETTING_TIME_LIMIT_FROM_RACE_START');
 
         return $this->render('userResultBets/detail.html.twig', [
             'resultsForUser' => $resultsForUser,
-            'user' => $user,
-            'season' => $season
+            'forUser' => $forUser,
+            'loggedInUser' => $user,
+            'season' => $season,
+            'now' => $now,
+            'bettingTimeLimit' => $bettingTimeLimit
         ]);
     }
 }
